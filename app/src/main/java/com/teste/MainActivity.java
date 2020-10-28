0c796f7740d86e18808b305cc02ad01b4c454e92
jardianoalmeida <jardiano.almeida@gmail.com>
2020-10-28 17:10:58 -0400

    Melhorando layout

--- app/src/main/java/com/teste/MainActivity.java
+++ app/src/main/java/com/teste/MainActivity.java
@@ -11,10 +11,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.daimajia.numberprogressbar.NumberProgressBar;
 import com.squareup.picasso.Picasso;
+import com.teste.config.CircleTransform;
 import com.teste.config.RetrofitConfig;
+import com.teste.config.RoundedTransformation;
 import com.teste.model.Player;
 import com.teste.service.HTTPService;
 
@@ -29,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView country;
     private TextView pos;
     private TextView percentual;
+    private TextView cpDisputada;
+    private TextView cpVencida;
     private ImageView imgPerson;
     private NumberProgressBar numberProgressBar1;
     private NumberProgressBar numberProgressBar2;
@@ -39,6 +44,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         name = findViewById(R.id.name);
+        cpVencida = findViewById(R.id.copas_vencidas_pos);
+        cpDisputada = findViewById(R.id.copas_disputadas_pos);
         country = findViewById(R.id.country);
         pos = findViewById(R.id.pos);
         percentual = findViewById(R.id.percentual);
@@ -61,16 +68,21 @@ public class MainActivity extends AppCompatActivity {
         NetworkInfo netInfo = cm.getActiveNetworkInfo();
         if (netInfo != null && netInfo.isConnected()){
             Player player = new HTTPService().execute().get();
-            Picasso.with(context).load(player.getImg()).into(this.imgPerson);
-            System.out.println(player.getBarra().getCopaMundoVencida().getPla());
-            System.out.println(player.getBarra().getCopaMundoDisputada().getPla());
+            Picasso.with(context).load(player.getImg()).transform(new CircleTransform()).fit().into(this.imgPerson);
+
+            Double percentual = Double.parseDouble(player.getPercentual());
+            String resultado = String.format("%.3f", percentual);
 
             this.numberProgressBar1.setProgress(Integer.parseInt(player.getBarra().getCopaMundoDisputada().getPla()));
             this.numberProgressBar2.setProgress(Integer.parseInt(player.getBarra().getCopaMundoVencida().getPla()));
             this.name.setText(player.getName());
             this.country.setText(player.getCountry());
             this.pos.setText(player.getPos());
-            this.percentual.setText(player.getPercentual());
+            this.percentual.setText(resultado);
+            cpDisputada.setText(player.getBarra().getCopaMundoDisputada().getPos()+"º");
+            cpVencida.setText(player.getBarra().getCopaMundoVencida().getPos()+"º");
+        }else {
+            Toast.makeText(context, "No momento você sem conexão com a internet", Toast.LENGTH_SHORT).show();
         }
     }
 }
